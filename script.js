3325d0a13225a1f97c276b490ac343ffb08cd7b0
Donald Robinson II <donald0504@yahoo.com>
2016-10-28 09:16:04 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-function createParagraph() {
-  var para = document.createElement('p');
-  para.textContent = 'Hi there!';
-  document.body.appendChild(para);
-}
+var farenheit = 32;
+var a = 5/9;
+var b = farenheit - 32;
+var c = a * b;
+var today = "Hi, today the temperature in Farenheit is ";
+var todayCelsius = "The temperature in Celsius is ";
 
-var buttons = document.querySelectorAll('button');
+console.log(today + farenheit);
+console.log(todayCelsius + c);
 
-for(var i = 0; i < buttons.length ; i++) {
-  buttons[i].addEventListener('click', createParagraph);
-}
+
+var celsius = 0;
+var e = 9/5;
+var d = c * e;
+var g = d + 32;
+var todayC = "Hi, today's temperature in Celsius is ";
+var todayFarenheit = "The temperature in Farenheit is ";
+
+console.log(todayC + celsius);
+console.log(todayFarenheit + g);
+
+function toCelsius() {
+	var ltoday = document.getElementById ('today');
+	ltoday.innerHTML = today;
+};
